2d29de3ced71208759c260cb7db79dac17336fe7
gothi <shihaburabbi@gmail.com>
2021-01-12 19:09:25 +0600

    first commit

---
+++ src/QBConfig.js
@@ -0,0 +1,8 @@
+export default {
+  appId: '88347',
+  authKey: 'YUyLZPZVZjmkvRw',
+  authSecret: 'wSPMVVShebrYh-a',
+  accountKey: 'PqDPw1J8UB683B1JwH15',
+  apiEndpoint: '',
+  chatEndpoint: '',
+}
